322979a9ed71a5e48c5439d076d4db2981275e03
spatel1009 <spatel1009@gmail.com>
2018-12-20 14:53:34 -0500

    added markdown to portfolio body

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -8,4 +8,24 @@ module PortfoliosHelper
   def my_github
     link_to 'my personal github', 'https://www.github.com/spatel1009'
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language || :text).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
